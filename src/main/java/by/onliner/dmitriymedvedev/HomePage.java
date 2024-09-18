06279990ff15c4fae361f9f636797f49743d23a5
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-09-18 21:16:49 +0300

    add homepage xpath,Homepage class

---
+++ src/main/java/by/onliner/dmitriymedvedev/HomePage.java
@@ -0,0 +1,16 @@
+package by.onliner.dmitriymedvedev;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+    WebDriver driver;
+
+    public void clickButtonLogin() {
+        By buttonLoginBy = By.xpath(HomePageXPath.BUTTON_LOGIN_XPATH);
+        WebElement buttonLogin = driver.findElement(buttonLoginBy);
+        buttonLogin.click();
+    }
+
+}
