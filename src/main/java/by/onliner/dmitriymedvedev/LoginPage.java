28cf033d77f44f0bd1c4068b22c061b64832ad1a
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-09-18 20:20:53 +0300

    add loginpage xpath

--- src/main/java/by/onliner/dmitriymedvedev/LoginPage.java
+++ src/main/java/by/onliner/dmitriymedvedev/LoginPage.java
@@ -1,4 +1,11 @@
 package by.onliner.dmitriymedvedev;
 
 public class LoginPage {
+    public static final String INPUT_EMAIL_XPATH = "//input[@placeholder = 'Ник или e-mail']";
+    public static final String INPUT_PASSWORD_XPATH = "//input[@placeholder = 'Пароль']";
+    public static final String ERROR_EMAIL_XPATH = "//div[contains(text(),'Укажите ник или e-mail')]";
+
+    public static final String ERROR_PASSWORD_XPATH = "//div[contains(text(),'Укажите пароль')]";
+
+
 }
